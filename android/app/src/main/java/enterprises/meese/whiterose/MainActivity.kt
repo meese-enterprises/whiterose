76e23c987043caeb6e2bbe099ba33e91cab5272e
Aaron Meese <aaron@meese.dev>
2025-08-21 02:14:10 +0000

    fix: incorrect dependency Was preventing the project from compiling.

--- android/app/src/main/java/enterprises/meese/whiterose/MainActivity.kt
+++ android/app/src/main/java/enterprises/meese/whiterose/MainActivity.kt
@@ -4,11 +4,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.input.KeyboardOptions
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
